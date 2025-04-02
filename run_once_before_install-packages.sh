bbc48eac3bab4099b2d0d5b022480c019b22b156
Viktor Bekesi <viktor.bekesi@outlook.com>
2025-04-03 00:03:39 +0200

    Fixing installation script

--- run_once_before_install-packages.sh
+++ run_once_before_install-packages.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+# Bookworm backport repos
+echo "deb https://deb.debian.org/debian bookworm-backports main non-free-firmware" | sudo tee /etc/apt/sources.list
+echo "deb-src https://deb.debian.org/debian bookworm-backports main non-free-firmware" | sudo tee /etc/apt/sources.list
+echo "deb http://download.opensuse.org/repositories/home:/Alexx2000/Debian_12/ /" | sudo tee /etc/apt/sources.list
+echo "deb [arch=amd64,arm64,armhf] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list
+#echo "deb [signed-by=/usr/share/keyrings/teamviewer-keyring.gpg] https://linux.teamviewer.com/deb stable main" | sudo tee /etc/apt/sources.list
+
 # Install missing packages
 for pkg in $(cat package-list.txt); do
     if ! dpkg -l | grep -q "^ii  $pkg "; then
@@ -10,3 +17,11 @@ for pkg in $(cat package-list.txt); do
         echo "$pkg is already installed."
     fi
 done
+
+# Brave Browser
+curl -fsS https://dl.brave.com/install.sh | sh
+
+# TeamViewer
+wget -P ~/Downloads "https://download.teamviewer.com/download/linux/teamviewer_amd64.deb"
+sudo apt install ~/Downloads/teamviewer_amd64.deb
+
