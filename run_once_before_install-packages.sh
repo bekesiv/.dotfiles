077482a45e4b33bb05a14f016b708a3e93842e2f
Viktor Bekesi <viktor.bekesi@outlook.com>
2025-04-03 08:07:28 +0200

    Modifying lists

--- run_once_before_install-packages.sh
+++ run_once_before_install-packages.sh
@@ -1,29 +1,48 @@
 #!/bin/bash
 set -e
 
-# Bookworm backport repos
-echo "deb https://deb.debian.org/debian bookworm-backports main non-free-firmware" | sudo tee -a /etc/apt/sources.list
-echo "deb-src https://deb.debian.org/debian bookworm-backports main non-free-firmware" | sudo tee -a /etc/apt/sources.list
-echo "deb http://download.opensuse.org/repositories/home:/Alexx2000/Debian_12/ /" | sudo tee -a /etc/apt/sources.list
-echo "deb [arch=amd64,arm64,armhf] https://packages.microsoft.com/repos/code stable main" | sudo tee -a /etc/apt/sources.list
-#echo "deb [signed-by=/usr/share/keyrings/teamviewer-keyring.gpg] https://linux.teamviewer.com/deb stable main" | sudo tee -a /etc/apt/sources.list
+chezmoi_dir="$HOME/.local/share/chezmoi"
+
+# Function to check if a apt repository exists
+repo_exists() {
+  local repo="$1"
+  grep -Fxq "$repo" /etc/apt/sources.list /etc/apt/sources.list.d/*.list 2>/dev/null
+}
+
+# Adding apt repositories
+while IFS= read -r repo; do
+  # Skip empty lines and comments
+  [[ -z "$repo" || "$repo" =~ ^# ]] && continue
+
+  # Check if repo already exists
+  if repo_exists "$repo"; then
+    echo "✅ Repository already exists: $repo"
+  else
+    echo "➕ Adding repository: $repo"
+    sudo apt-add-repository "$repo"
+  fi
+done < "$chezmoi_dir/packages/apt-repositories.txt"
+
+# Update package lists
+echo "🔄 Updating APT sources..."
+sudo apt update
 
 # Remove not needed packages
-# sudo apt remove -y --autoremove --ignore-missing
+sudo apt remove -y --autoremove --ignore-missing $(tr '\n' ' ' < "$chezmoi_dir/packages/remove_packages.list")
 
-# Install missing packages
-for pkg in $(cat package-list.txt); do
-    if ! dpkg -l | grep -q "^ii  $pkg "; then
-        echo "Installing $pkg..."
-        sudo apt-get install -y $pkg
-    else
-        echo "$pkg is already installed."
-    fi
-done
+# # Install missing packages
+# for pkg in $(cat package-list.txt); do
+#     if ! dpkg -l | grep -q "^ii  $pkg "; then
+#         echo "Installing $pkg..."
+#         sudo apt-get install -y $pkg
+#     else
+#         echo "$pkg is already installed."
+#     fi
+# done
 
 # Flatpaks
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install -y $(tr '\n' ' ' < "$chezmoi_dir/packages/flatpak.list")
 flatpak uninstall --unused
 
 
@@ -40,4 +59,4 @@ sudo apt install ~/Downloads/teamviewer_amd64.deb
 sudo apt autoremove
 
 #Gnome settings
-dconf load / ~/.local/share/chezmoi/dconf_settings_dump.dconf
+dconf load / "$chezmoi_dir/dconf_settings_dump.dconf"
