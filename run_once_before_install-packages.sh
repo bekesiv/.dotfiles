c292d3b49b7bff7d1014b9e990574cda64b75de5
Viktor Bekesi <viktor.bekesi@outlook.com>
2025-04-03 07:09:07 +0200

    Adding install and remove packages list and flatpak list

--- run_once_before_install-packages.sh
+++ run_once_before_install-packages.sh
@@ -8,6 +8,9 @@ echo "deb http://download.opensuse.org/repositories/home:/Alexx2000/Debian_12/ /
 echo "deb [arch=amd64,arm64,armhf] https://packages.microsoft.com/repos/code stable main" | sudo tee -a /etc/apt/sources.list
 #echo "deb [signed-by=/usr/share/keyrings/teamviewer-keyring.gpg] https://linux.teamviewer.com/deb stable main" | sudo tee -a /etc/apt/sources.list
 
+# Remove not needed packages
+# sudo apt remove -y --autoremove --ignore-missing
+
 # Install missing packages
 for pkg in $(cat package-list.txt); do
     if ! dpkg -l | grep -q "^ii  $pkg "; then
@@ -25,3 +28,4 @@ curl -fsS https://dl.brave.com/install.sh | sh
 wget -P ~/Downloads "https://download.teamviewer.com/download/linux/teamviewer_amd64.deb"
 sudo apt install ~/Downloads/teamviewer_amd64.deb
 
+sudo apt autoremove
